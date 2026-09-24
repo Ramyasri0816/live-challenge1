45b493eafe2ab4850547fe14e18f7c59ceaae3de
Ramyasri0816 <vu.241fa04d11@gmail.com>
2026-09-24 12:06:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function startLearning() {
+
+    alert("Welcome to StudyFlow AI! 🚀");
+
+}
+
+
+function generateNotes() {
+
+    alert("AI is preparing your smart revision notes ✨");
+
+}
+
+
+function startExamPrep() {
+
+    alert("Your personalized exam preparation is starting! 📚");
+
+}
